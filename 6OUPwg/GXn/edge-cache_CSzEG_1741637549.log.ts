1a4a6a9edab02d1b176332b4d1057c137ee18bc2
aid584ENZ <archers.lap_0k@icloud.com>
2025-03-12 02:32:31 +0000

    Add user registration feature to the file

--- 6OUPwg/GXn/edge-cache_CSzEG_1741637549.log.ts
+++ 6OUPwg/GXn/edge-cache_CSzEG_1741637549.log.ts
@@ -1,5 +1,4 @@
 import { withClerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
 
 export default withClerkMiddleware((req) => {
   return NextResponse.next({
